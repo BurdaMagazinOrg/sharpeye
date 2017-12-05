260c43b4969115b4a84df67a595b6e10b7cc1c51
Volker Killesreiter <killesre@gmail.com>
2017-12-05 15:08:17 +0100

    move to named function

--- specs/tasks.js
+++ specs/tasks.js
@@ -92,26 +92,7 @@ function processAction(action) {
     }
 
     if (action.dragAndDrop !== undefined){
-
-      browser.execute(function(elemXpath, offsetX, offsetY) {
-        // Drag element in document with defined offset position.
-        // We have to fake this since browser.moveTo() is not working for
-        // firefox.
-        let fireMouseEvent = function (type, element, x, y) {
-          let event = document.createEvent('MouseEvents')
-          event.initMouseEvent(type, true, (type !== 'mousemove'), window, 0, 0, 0, x, y, false, false, false, false, 0, element)
-          element.dispatchEvent(event)
-        }
-
-        let dragElement = document.evaluate(elemXpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
-        let pos = dragElement.getBoundingClientRect()
-        let centerX = Math.floor((pos.left + pos.right) / 2)
-        let centerY = Math.floor((pos.top + pos.bottom) / 2)
-        fireMouseEvent('mousedown', dragElement, centerX, centerY)
-        fireMouseEvent('mousemove', document, centerX + offsetX, centerY + offsetY)
-        fireMouseEvent('mouseup', dragElement, centerX + offsetX, centerY + offsetY)
-
-      }, action.dragAndDrop, action.offsetx, action.offsety)
+      browser.execute(dragAndDrop(), action.dragAndDrop, action.offsetx, action.offsety)
     }
 
     if (action.wait) {
@@ -160,3 +141,25 @@ function slashToUnderscore(string) {
   string = string.substr(1)
   return string
 }
+
+function dragAndDrop() {
+  return function(elemXpath, offsetX, offsetY) {
+    // Drag element in document with defined offset position.
+    // We have to fake this since browser.moveTo() is not working for
+    // firefox.
+    let fireMouseEvent = function (type, element, x, y) {
+      let event = document.createEvent('MouseEvents')
+      event.initMouseEvent(type, true, (type !== 'mousemove'), window, 0, 0, 0, x, y, false, false, false, false, 0, element)
+      element.dispatchEvent(event)
+    }
+
+    let dragElement = document.evaluate(elemXpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
+    let pos = dragElement.getBoundingClientRect()
+    let centerX = Math.floor((pos.left + pos.right) / 2)
+    let centerY = Math.floor((pos.top + pos.bottom) / 2)
+    fireMouseEvent('mousedown', dragElement, centerX, centerY)
+    fireMouseEvent('mousemove', document, centerX + offsetX, centerY + offsetY)
+    fireMouseEvent('mouseup', dragElement, centerX + offsetX, centerY + offsetY)
+  }
+}
+
