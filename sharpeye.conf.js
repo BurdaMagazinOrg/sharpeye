5a11e8267f1ab1c29741c6e135cf9a9839c5e43d
Volker Killesreiter <killesre@gmail.com>
2020-02-11 09:51:33 +0100

    revert timeout change

--- sharpeye.conf.js
+++ sharpeye.conf.js
@@ -148,7 +148,7 @@ exports.config = {
   baseUrl: "http://localhost",
   //
   // Default timeout for all waitFor* commands.
-  waitforTimeout: 20000,
+  waitforTimeout: 10000,
   //
   // Default timeout in milliseconds for request
   // if browser driver or grid doesn't send response
