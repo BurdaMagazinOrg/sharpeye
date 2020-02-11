4f61683782d284c897e3ef02f464a691cf2c1e1e
Volker Killesreiter <killesre@gmail.com>
2020-02-11 09:58:21 +0100

    copy global options

--- src/takeScreenshot.js
+++ src/takeScreenshot.js
@@ -12,9 +12,7 @@ const replaceContent = require("./replaceContent")
  * @see https://github.com/wswebcreation/webdriver-image-comparison
  */
 const takeScreenshot = task => {
-  let screenshotOptions = {}
-  // Merge global options
-  Object.assign(screenshotOptions, options)
+  let screenshotOptions = options || {}
 
   if (typeof task === "object") {
 
