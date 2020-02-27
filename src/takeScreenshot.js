36517b7e71d3c346fe22405087711c134b629b5a
Volker Killesreiter <killesre@gmail.com>
2020-02-27 14:28:36 +0100

    linted

--- src/takeScreenshot.js
+++ src/takeScreenshot.js
@@ -1,10 +1,16 @@
 const assert = require("assert")
-const options = require("../sharpeye.conf").options
+const { options } = require("../sharpeye.conf")
 
 const alignHeight = require("./alignHeight")
 const isXPath = require("./isXPath")
 const replaceContent = require("./replaceContent")
 
+const assertDiff = result => {
+  assert.ok(
+    result <= options.misMatchTolerance,
+    `Screenshot differs from reference by ${result}%.`
+  )
+}
 
 /**
  * Use webdriver image comparison service.
@@ -12,17 +18,21 @@ const replaceContent = require("./replaceContent")
  * @see https://github.com/wswebcreation/webdriver-image-comparison
  */
 const takeScreenshot = task => {
-  let screenshotOptions = Object.create(options || {})
+  const screenshotOptions = Object.create(options || {})
 
   if (typeof task === "object") {
-
     if (task.tolerance) {
       screenshotOptions.misMatchTolerance = task.tolerance
     }
 
     if (task.replace) {
       task.replace.forEach(entry => {
-        browser.execute(replaceContent(), entry.$, entry.value, isXPath(entry.$))
+        browser.execute(
+          replaceContent(),
+          entry.$,
+          entry.value,
+          isXPath(entry.$)
+        )
       })
     }
 
@@ -38,6 +48,7 @@ const takeScreenshot = task => {
       })
     }
 
+    // Multiple viewports.
     if (task.viewports) {
       task.viewports.forEach(viewport => {
         alignHeight(viewport.width, viewport.height)
@@ -45,9 +56,13 @@ const takeScreenshot = task => {
         assertDiff(browser.checkFullPageScreen(task.tag, screenshotOptions))
       })
 
+      // Single element.
     } else if (task.element) {
-      assertDiff(browser.checkElement($(task.element), task.tag, screenshotOptions))
+      assertDiff(
+        browser.checkElement($(task.element), task.tag, screenshotOptions)
+      )
 
+      // Full page with resize.
     } else if (task.fullPage) {
       // Let things settle a bit before calculating desired height.
       browser.pause(task.pause || 0)
@@ -56,22 +71,16 @@ const takeScreenshot = task => {
       browser.pause(task.pause || 0)
       assertDiff(browser.checkFullPageScreen(task.tag, screenshotOptions))
 
+      // Curren screen.
     } else {
       browser.pause(task.pause || 0)
       assertDiff(browser.checkScreen(task.tag, screenshotOptions))
     }
 
-  // Deprecated: task should be object not path only.
+    // Deprecated: task should be object not path only.
   } else {
     assertDiff(browser.checkFullPageScreen(task, screenshotOptions))
   }
-}
-
-const assertDiff = (result) => {
-  assert.ok(
-    result <= options.misMatchTolerance, 
-    "Screenshot differs from reference by " + result + "%."
-  )
 }
 
 module.exports = takeScreenshot
