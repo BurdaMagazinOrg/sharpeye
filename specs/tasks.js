ffa5f8955a436bd2332db012c1f777248759f372
Volker Killesreiter <killesre@gmail.com>
2020-02-28 10:10:53 +0100

    linted tasks.js

--- specs/tasks.js
+++ specs/tasks.js
@@ -1,11 +1,11 @@
 const tasks = require("../sharpeye.tasks")
-const options = require("../sharpeye.conf").options
-const baseUrl = options.baseUrl
-
 const alignHeight = require("../src/alignHeight")
 const processAction = require("../src/processAction")
 const sanitizeTag = require("../src/sanitizeTag")
 const takeScreenshot = require("../src/takeScreenshot")
+const { options } = require("../sharpeye.conf")
+
+const { baseUrl } = options
 
 describe("Task", function() {
   this.retries(options.numRetries)
@@ -16,32 +16,33 @@ describe("Task", function() {
   tasks.forEach(function(task) {
     // Check, if actions, or next page call.
     if (typeof task === "object") {
-      it(task.path + (task.name ? " -> " + task.name : "") + ": should look good", function () {
+      it(`${task.path} ${
+        task.name ? `-> ${task.name}` : ""
+      }: should look good`, function() {
+        // Set viewport size before running task.
         if (task.viewports) {
           alignHeight(task.viewports[0].width, task.viewports[0].height)
         }
         browser.url(baseUrl + task.path)
-        task.tag = sanitizeTag(task.path + (task.name ? "-" + task.name : ""))
+        task.tag = sanitizeTag(
+          `${task.path}${task.name ? `-${task.name}` : ""}`
+        )
         task.misMatchTolerance = options.misMatchTolerance
         // Go through all actions.
-        let actions = task.actions ? task.actions : []
-        actions.forEach(
-          entry => processAction(entry)
-        )
+        const actions = task.actions ? task.actions : []
+        actions.forEach(entry => processAction(entry))
 
         // Take a screenshot after actions.
         if (!task.noScreenshot) {
           takeScreenshot(task)
         }
       })
-    }
-    else if (task === "reload") {
-      it(task + ": should reload", () => {
+    } else if (task === "reload") {
+      it(`${task}: should reload`, () => {
         browser.refresh()
       })
-    }
-    else {
-      it(sanitizeTag(task) + ": should look good", function () {
+    } else {
+      it(`${sanitizeTag(task)}: should look good`, function() {
         // Open next page.
         browser.url(baseUrl + task)
         takeScreenshot(sanitizeTag(task))
