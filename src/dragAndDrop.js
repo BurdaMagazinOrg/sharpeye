250511add3f0a9c8513bf3dec8cb9dafc48cde88
Volker Killesreiter <killesre@gmail.com>
2020-03-02 12:33:54 +0100

    use const

--- src/dragAndDrop.js
+++ src/dragAndDrop.js
@@ -4,8 +4,8 @@ const dragAndDrop = () => {
     // Drag element in document with defined offset position.
     // We have to fake this since browser.moveTo() is not working for
     // firefox.
-    var fireMouseEvent = function(type, element, x, y) {
-      var event = document.createEvent("MouseEvents")
+    let fireMouseEvent = function(type, element, x, y) {
+      let event = document.createEvent("MouseEvents")
       event.initMouseEvent(
         type,
         true,
@@ -25,10 +25,7 @@ const dragAndDrop = () => {
       )
       element.dispatchEvent(event)
     }
-    var centerX
-    var centerY
-    var dragElement
-    var pos
+    let dragElement
 
     if (isXPath) {
       dragElement = document.evaluate(
@@ -41,9 +38,9 @@ const dragAndDrop = () => {
     } else {
       dragElement = document.querySelector(selector)
     }
-    pos = dragElement.getBoundingClientRect()
-    centerX = Math.floor((pos.left + pos.right) / 2)
-    centerY = Math.floor((pos.top + pos.bottom) / 2)
+    const pos = dragElement.getBoundingClientRect()
+    const centerX = Math.floor((pos.left + pos.right) / 2)
+    const centerY = Math.floor((pos.top + pos.bottom) / 2)
     fireMouseEvent("mousedown", dragElement, centerX, centerY)
     fireMouseEvent("mousemove", document, centerX + offsetX, centerY + offsetY)
     fireMouseEvent("mouseup", dragElement, centerX + offsetX, centerY + offsetY)
